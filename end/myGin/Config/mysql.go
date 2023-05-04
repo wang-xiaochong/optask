d01465a419ee3535b111e9db71b13a4c45517b5f
1378334435@qq.com <1378334435@qq.com>
2023-05-04 22:48:27 +0800

    code

--- end/myGin/Config/mysql.go
+++ end/myGin/Config/mysql.go
@@ -11,12 +11,12 @@ type MysqlConfig struct {
 func GetMysqlConfig() *MysqlConfig {
 
 	ret := &MysqlConfig{
-		Account:    "xiaochong",
-		Password:   "Lrb7MMY3AjKDGyGz",
-		RemoteIP:   "43.138.81.135",
-		// Account:    "root",
-		// Password:   "xs000809",
-		// RemoteIP:   "127.0.0.1",
+		// Account:    "xiaochong",
+		// Password:   "Lrb7MMY3AjKDGyGz",
+		// RemoteIP:   "43.138.81.135",
+		Account:    "root",
+		Password:   "xs000809",
+		RemoteIP:   "127.0.0.1",
 		RemotePort: "3306",
 		Database:   "xiaochong",
 	}
